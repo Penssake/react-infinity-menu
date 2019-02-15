dd88e65329e2bab163bf01cfd757954aea3135eb
penssake <penssake@gmail.com>
2019-02-15 12:33:26 -0800

    update

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -7,14 +7,19 @@ import Nav from '../navContainer'
 import {data} from '../../data/nav-data'
 import Hamburger from '../hamburger'
 
+import './app.scss'
+
 const App = (props) => {
   return (
     <BrowserRouter>
-      <div className='app'>
-        <Header />
-        <Hamburger />
-        <Nav data={data} />
-        <Route exact path='/' component={Landing}></Route>
+      <div>
+        <div className='app-overlay'><h1>Currently only meant to size to 700px</h1></div>
+        <div className='app'>
+          <Header />
+          <Hamburger />
+          <Nav data={data} />
+          <Route exact path='/' component={Landing}></Route>
+        </div>
       </div>
     </BrowserRouter>
   )
